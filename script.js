8851e69b8752c1148cd19fe30cd9a14d7124f0fb
Rob Cranfill <robcranfill@robcranfill.net>
2023-05-04 11:00:47 -0700

    Better!

--- script.js
+++ script.js
@@ -16,6 +16,53 @@ reset() {
       console.info("got NW ok! in computeAndDisplay")
     }
     netWeightDisplay.innerHTML = 'calc!'
+
+    // why not just get the objects now?
+
+
+    const fgw_obj = document.querySelector('div[name="fgw"]')
+    if (fgw_obj === null) {
+      console.error("FGW IS NULL! in computeAndDisplay")
+      return
+    }
+    const fgw = fgw_obj.innerText
+    console.info("fgw: " + fgw)
+
+    const cgw_obj = document.querySelector('input[name="cgw"]')
+    if (cgw_obj === null) {
+      console.error("CGW IS NULL! in computeAndDisplay")
+      return
+    }
+    const cgw = cgw_obj.value
+    console.info("cgw: " + cgw)
+
+    
+    const ew_obj = document.querySelector('div[name="ew"]')
+    if (ew_obj === null) {
+      console.error("EW IS NULL! in computeAndDisplay")
+      return
+    }
+    const ew = ew_obj.innerText
+    console.info("ew: " + ew)
+
+    // the calculation:
+    var cnw = cgw - ew
+    console.info("CURRENT NET WEIGHT: " + cnw)
+
+    
+    const fnw_obj = document.querySelector('div[name="fnw"]')
+    if (fnw_obj === null) {
+      console.error("FNW IS NULL! in computeAndDisplay")
+      return
+    }
+    const fnw = fnw_obj.innerText
+    console.info("fnw: " + fnw)
+    
+    ff = cnw / fnw
+    console.info("ff: " + ff + "%")
+    
+    netWeightDisplay.innerHTML = cnw
+
   }
 
 
@@ -59,7 +106,7 @@ else {
 }
 
 // const numberButtons = document.querySelectorAll('[data-number]')
-const netWeightDisplay =  document.querySelector('div[name="cnw"]')
+const netWeightDisplay = document.querySelector('div[name="cnw"]')
 if (netWeightDisplay === null) {
   console.error("NW IS NULL!")
 }
