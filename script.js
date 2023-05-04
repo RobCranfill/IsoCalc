b88709d9160682b7010cbecc323b3c6bd04b8817
Rob Cranfill <robcranfill@robcranfill.net>
2023-05-04 11:41:28 -0700

    Remove dead code

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ class IsoCalc {
     // this.reset()
   }
 
-reset() {
-  this.netWeightDisplay.innerHTML = 'OK!'
-}
-
   computeAndDisplay() {
     console.info("computeAndDisplay!")
+
+    const calcButton = document.querySelector('button[type="submit"]')
+    // const calcButton = document.querySelector('button[type="submit"], button:not([type])')
+    if (calcButton === null) {
+      console.error("BUTTON IS NULL!")
+      }
+    else {
+      console.info("got button ok!")
+      }
+
+    // const numberButtons = document.querySelectorAll('[data-number]')
+    const netWeightDisplay = document.querySelector('div[name="cnw"]')
+    if (netWeightDisplay === null) {
+      console.error("NW IS NULL!")
+      }
+    else {
+      console.info("got NW ok!")
+      }
+
+
     if (netWeightDisplay === null) {
       console.error("NW IS NULL! in computeAndDisplay")
     }
@@ -56,7 +72,7 @@ reset() {
       return
     }
     const fnw = fnw_obj.innerText
-    console.info("fnw: (WTF?) " + fnw)
+    console.info("fnw: " + fnw)
 
     const ff = Math.trunc((cnw / fnw) * 100)
     console.info("ff: " + ff + "%")
@@ -71,70 +87,11 @@ reset() {
     
     netWeightDisplay.innerHTML = cnw
   }
-
-
-  getDisplayNumber(number) {
-    const stringNumber = number.toString()
-    const integerDigits = parseFloat(stringNumber.split('.')[0])
-    const decimalDigits = stringNumber.split('.')[1]
-    let integerDisplay
-    if (isNaN(integerDigits)) {
-      integerDisplay = ''
-    } else {
-      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
-    }
-    if (decimalDigits != null) {
-      return `${integerDisplay}.${decimalDigits}`
-    } else {
-      return integerDisplay
-    }
-  }
-
-  updateDisplay() {
-    this.currentOperandTextElement.innerText =
-      this.getDisplayNumber(this.currentOperand)
-    if (this.operation != null) {
-      this.previousOperandTextElement.innerText =
-        `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
-    } else {
-      this.previousOperandTextElement.innerText = ''
-    }
-  }
 }
-
-
-const calcButton = document.querySelector('button[type="submit"]')
-// const calcButton = document.querySelector('button[type="submit"], button:not([type])')
-if (calcButton === null) {
-  console.error("BUTTON IS NULL!")
-}
-else {
-  console.info("got button ok!")
-}
-
-// const numberButtons = document.querySelectorAll('[data-number]')
-const netWeightDisplay = document.querySelector('div[name="cnw"]')
-if (netWeightDisplay === null) {
-  console.error("NW IS NULL!")
-}
-else {
-  console.info("got NW ok!")
-}
-
-// TODO: const percentDisplay =  document.querySelector('[net_weight]')
 
 
 const isocalc = new IsoCalc()
 
-
 calcButton.addEventListener('click', button => {
   isocalc.computeAndDisplay()
 })
-
-
-// numberButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     calculator.appendNumber(button.innerText)
-//     calculator.updateDisplay()
-//   })
-// })
